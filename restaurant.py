457147b95b5cf25857962335e4bfef91b2194b83
Spoorthika Reddy Legala <154603932+spoorthika@users.noreply.github.com>
2026-04-06 08:41:14 +0530

    Add files via upload

--- restaurant.py
+++ restaurant.py
@@ -4,48 +4,8 @@ import mysql.connector
 app = Flask(__name__)
 password = input("Enter MySQL password: ")
 
-queries = {
-    "orders": """
-        SELECT
-            order_id,
-            DATE_FORMAT(order_date, '%Y-%m-%d') AS order_date,
-            GROUP_CONCAT(item_id SEPARATOR ', ') AS items,
-            GROUP_CONCAT(qty SEPARATOR ', ') AS quantity,
-            SUM(total) AS order_total
-        FROM orders_history
-        GROUP BY order_id, order_date
-        ORDER BY order_id
-    """,
-    "payments": """
-        SELECT
-            payment_id,
-            order_id,
-            DATE_FORMAT(payment_date, '%Y-%m-%d') AS payment_date,
-            payment_type,
-            amount_due,
-            tips,
-            discount,
-            total_paid,
-            payment_status
-        FROM payments
-    """,
-    "menu": """
-        SELECT
-            menu.item_id,
-            menu.item_name,
-            categories.category_name,
-            menus.menu_name,
-            menu.size,
-            menu.price
-        FROM menu
-        JOIN categories ON menu.cat_id = categories.cat_id
-        JOIN menus ON menu.menu_id = menus.menu_id
-        ORDER BY menu.item_id
-    """
-}
 
-
-def get_data(name):
+def get_data(query):
     conn = mysql.connector.connect(
         host="localhost",
         user="root",
@@ -53,15 +13,10 @@ def get_data(name):
         database="restaurant"
     )
     cursor = conn.cursor(dictionary=True)
-    cursor.execute(queries[name])
+    cursor.execute(query)
     data = cursor.fetchall()
     cursor.close()
     conn.close()
-
-    for row in data:
-        for key, value in row.items():
-            row[key] = "" if value is None else str(value)
-
     return data
 
 
@@ -70,15 +25,56 @@ def home():
     return send_file('index.html')
 
 
-@app.route('/<name>')
-def show_data(name):
-    if name not in queries:
-        return jsonify({"error": "Page not found"}), 404
+@app.route('/orders')
+def orders():
+    query = """
+        SELECT
+            order_id,
+            DATE_FORMAT(order_date, '%Y-%m-%d') AS order_date,
+            GROUP_CONCAT(item_id SEPARATOR ', ') AS items,
+            GROUP_CONCAT(qty SEPARATOR ', ') AS quantity,
+            IFNULL(CAST(SUM(total) AS CHAR), '') AS order_total
+        FROM orders_history
+        GROUP BY order_id, order_date
+        ORDER BY order_id
+    """
+    return jsonify(get_data(query))
 
-    try:
-        return jsonify(get_data(name))
-    except Exception as error:
-        return jsonify({"error": str(error)}), 500
+
+@app.route('/payments')
+def payments():
+    query = """
+        SELECT
+            payment_id,
+            order_id,
+            DATE_FORMAT(payment_date, '%Y-%m-%d') AS payment_date,
+            payment_type,
+            IFNULL(CAST(amount_due AS CHAR), '') AS amount_due,
+            IFNULL(CAST(tips AS CHAR), '') AS tips,
+            IFNULL(CAST(discount AS CHAR), '') AS discount,
+            IFNULL(CAST(total_paid AS CHAR), '') AS total_paid,
+            payment_status
+        FROM payments
+    """
+    return jsonify(get_data(query))
+
+
+@app.route('/menu')
+def menu():
+    query = """
+        SELECT
+            menu.item_id,
+            menu.item_name,
+            categories.category_name,
+            menus.menu_name,
+            IFNULL(menu.size, '') AS size,
+            IFNULL(CAST(menu.price AS CHAR), '') AS price
+        FROM menu
+        JOIN categories ON menu.cat_id = categories.cat_id
+        JOIN menus ON menu.menu_id = menus.menu_id
+        ORDER BY menu.item_id
+    """
+    return jsonify(get_data(query))
 
 
 if __name__ == '__main__':
